a7820575c649d84f3fd06f1effecd058d5f8c636
razantayyem <razanr543@gmail.com>
2018-12-04 12:31:27 +0200

    add src/server/server.js file #2

---
+++ src/server/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const router = require('./router.js');
+
+const PORT = 8001;
+
+const server = http.createServer(router);
+server.listen(PORT, () => {
+	console.log(` the server up and running on port ${PORT}!`);
+});
