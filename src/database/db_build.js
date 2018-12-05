c5255475b52c5a5a10dab18ab8aa09a9d01415b8
razantayyem <razanr543@gmail.com>
2018-12-05 10:45:39 +0200

    use path module and delete result #3

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -1,9 +1,10 @@
 const fs = require ("fs");
+const path = require('path');
 const db_connection = require ("./db_connection");
 
-const build_file =fs.readFileSync(__dirname + "/db_build.sql", "utf8");
+const build_file =fs.readFileSync(path.join(__dirname, '/db_build.sql'), 'utf-8');
 
-db_connection.query(build_file, (error, result) =>{
+db_connection.query(build_file, error =>{
   if (error){
     console.log ("failed", error);
   } else {
